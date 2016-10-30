976aaeca4e0daf1e63f86e39f9187a4f127b605c
AlyonaLinnovaa <alyonalinnova@gmail.com>
2016-10-30 21:03:25 +0700

    Update index2.php

--- index2.php
+++ index2.php
@@ -2,18 +2,19 @@
 	<Head><meta charset=”utf-8”></Head>
 	<body>
 	<form  method="GET">
+	Перевод из полярных координат в декартовые:
 	<p><img src="photo.jpg" width="241" height="239" alt="" /> </p>
-	Длина вектра = <input type="text" name="vector" value="<?php
+	Длина вектора = <input type="text" name="vector" value="<?php
 		if (isset($_GET['vector'])){
 			echo htmlspecialchars ($_GET['vector']);
 		}
 	    ?>">
-	Угол =<input type="text" name="angle" value="<?php
+	Угол(радиан) =<input type="text" name="angle" value="<?php
 		if (isset($_GET['angle'])){
 			echo htmlspecialchars ($_GET['angle']);
 		}
 	   ?>">
-<input type="submit" name="compute" value="Вычислить">
+Декортовые координаты=<input type="submit" name="compute" value="Вычислить">
 </form>
 
 <?php
@@ -27,8 +28,7 @@
 		if (isset($_GET['vector']) && isset($_GET['angle'])) {
 			if ((!(is_numeric($_GET['vector'])))||(!(is_numeric($_GET['angle'])))) {
 				echo 'Значение координат может быть только числом!';
-			} elseif (($_GET['vector']<0)||($_GET['angle']<0)) {
-				echo "Значение координат должно быть не отрицательным!";} else {
+			} else {
 				echo number_format((dot_x($_GET['vector'],$_GET['angle'])),2,',','')."</br>";
 				echo number_format((dot_y($_GET['vector'],$_GET['angle'])),2,',','');
 		
